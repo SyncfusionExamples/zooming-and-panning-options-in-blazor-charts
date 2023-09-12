4441a870ed913f542540fd8ee9ccdefeee7f8e9b
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2023-09-12 13:41:00 +0530

    update project

--- Program.cs
+++ Program.cs
@@ -1,29 +1,33 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using ZoomingAndPanning.Data;
 using Syncfusion.Blazor;
 
-namespace Blazor_Charts
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTEwODE1QDMxMzkyZTMzMmUzMERtdE1OS3Jld3ZnaWFDaUZMa1dNa0srcDVYQ2NrNmdTUVFKREhleXhmS0E9");
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            builder.Services.AddSyncfusionBlazor();
-
-            await builder.Build().RunAsync();
-        }
-    }
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
